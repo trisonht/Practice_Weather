6e2ac9af5e09e90c035646c1c0f29e424b5c9180
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-09-07 10:07:39 +0700

    NEW: Practice_DAWD_Android\Weather_V2 - Add dependencies: volley, gson, glide

---
+++ Weather_V2/app/src/main/java/hieu_iceTea/weather_V2/controller/VolleyAppController.java
@@ -0,0 +1,62 @@
+package hieu_iceTea.weather_V2.controller;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyAppController extends Application {
+
+    private static VolleyAppController instance;
+    private RequestQueue requestQueue;
+    //private static Context ctx;
+
+    //Vì [extends Application] nên không cần cái này:
+    /*private AppController(Context context) {
+
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+    }*/
+
+    public static synchronized VolleyAppController getInstance() {
+
+        //Vì [extends Application] nên không cần cái này:
+        /*if (instance == null) {
+            instance = new AppController(context);
+        }*/
+
+        return instance;
+
+    }
+
+    public RequestQueue getRequestQueue() {
+
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return requestQueue;
+
+    }
+
+    public <T> void addToRequestQueue(Request<T> request) {
+
+        getRequestQueue().add(request);
+
+    }
+
+    //Vì [extends Application] nên THÊM cái này:
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        instance = this;
+
+    }
+
+}
